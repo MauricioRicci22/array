bfffbc367131e45011f8db1d1f3c51449a67021b
MauricioRicci22 <119349779+MauricioRicci22@users.noreply.github.com>
2022-11-28 15:09:15 -0300

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,17 +1,17 @@
 C=[]
-cont=0
-prom=0
+conta=0
+prome=0
 Cant=int(input("Cuántos medidas de temperatura va a asignar? "))
 
 for i in range (0, Cant):
     Temp=int(input("Temperatura: "))
-    prom+=Temp
+    prome+=Temp
     C.append(Temp)
 
-prom=prom/Cant
+prome=prom/Cant
 
 for i in range (0, Cant):
-    if (C[i]>=prom):
+    if (C[i]>=prome):
         cont+=1
 
 print(C)
